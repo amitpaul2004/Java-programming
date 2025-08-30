17e7d33188b246f8f7cfcb47d762483e8f7b9bf8
amitpaul2004 <pal900290@gmail.com>
2025-08-31 00:00:53 +0530

    new

--- javabasic.java
+++ javabasic.java
@@ -2,7 +2,7 @@ public class javabasic {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a);
-        System.out.println(b);
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
