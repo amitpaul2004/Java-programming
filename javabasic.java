4f26360843e585c81ca936ff5c3378904dd3b610
amitpaul2004 <pal900290@gmail.com>
2025-08-30 22:27:56 +0530

    new

--- javabasic.java
+++ javabasic.java
@@ -1,5 +1,7 @@
 public class javabasic {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
