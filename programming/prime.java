28353e0d1ea9779ab3727e628677b42ebed7de21
amitpaul2004 <pal900290@gmail.com>
2025-09-01 23:29:10 +0530

    prime

---
+++ programming/prime.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;  // 0 and 1 are not prime
+        } else {
+            // Check divisibility
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        // Print result
+        if (isPrime) {
+            System.out.println(num + " is a Prime number.");
+        } else {
+            System.out.println(num + " is NOT a Prime number.");
+        }
+
+        sc.close();
+    }
+}
