b285aef9a580350b00db9620168cf86a6d6dfbf6
amitpaul2004 <pal900290@gmail.com>
2025-09-07 23:50:00 +0530

    if-else

--- javaBasic/javabasic.java
+++ javaBasic/javabasic.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class javabasic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println(input);    
+        char ch='a';
+        int n=ch;
+        System.out.println(n);
     }
 }
 
