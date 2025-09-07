95ec930412af3f678492dbb71dceac63699a2c52
amitpaul2004 <pal900290@gmail.com>
2025-09-08 00:08:29 +0530

    narea of circle

--- javaBasic/area_of_circle.java
+++ javaBasic/area_of_circle.java
@@ -1,7 +1,7 @@
 import java.util.*;
-public class javabasic {
+public class area_of_circle {
     public static void main(String[] args) {
-      scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
       float r = sc.nextFloat();
       float area = 3.14f * r * r;
       System.out.println(area);
