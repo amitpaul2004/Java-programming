19ded3dc77cd201286fa0595d3438bcf1fc890aa
amitpaul2004 <pal900290@gmail.com>
2025-09-07 23:22:56 +0530

    Update javabasic.java

--- javabasic.java
+++ javabasic.java
@@ -1,10 +1,14 @@
 import java.util.*;
 public class javabasic {
     public static void main(String[] args) {
-     char ch = 'a';
-     int no =ch;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.println(no);    
+        int avg = (a+b+c)/3;
+
+        System.out.println(avg);    
     }
 }
 
