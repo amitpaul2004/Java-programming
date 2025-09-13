3eb071c499dcdd4f8ab69ed4da7f117c8bec02ce
amitpaul2004 <pal900290@gmail.com>
2025-09-13 09:11:02 +0530

    new one

---
+++ javaBasic/coditional statment/larg_2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class larg_2{
+    public static void main (String args []){
+        int A=1;
+        int B=2;
+        if (A>B){
+            System.out.println("A is greater");
+        }
+        else{
+            System.out.println("B is greater");
+        }
+    }
+}
