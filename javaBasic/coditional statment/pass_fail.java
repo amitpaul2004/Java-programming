bf620e456db285cb57ff0ee579f84ff8e62cc761
amitpaul2004 <pal900290@gmail.com>
2025-09-26 20:13:58 +0530

    pass fail

---
+++ javaBasic/coditional statment/pass_fail.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class pass_fail {
+    public static void main(String[] args) {
+     int marks=30;
+     String rc=marks>=33?"pass":"fail";
+     System.out.println(rc);
+    }
+}
