89a6b049d011361313b40a4d1104f50151a1f8f0
amitpaul2004 <pal900290@gmail.com>
2025-09-26 21:21:48 +0530

    temp

---
+++ practice/temp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class temp{
+     public static void main(String[]args) {
+        double temp=103.5;
+      if (temp>100){
+            System.out.println("fever");
+        }
+        else{
+            System.out.println("no fever");
+        }
+}
+}
