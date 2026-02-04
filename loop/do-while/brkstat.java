08b5cb7df01ec3f8bc940581a8d9e8288d658eb5
amitpaul2004 <pal900290@gmail.com>
2026-02-04 20:55:47 +0530

    continue statment

---
+++ loop/do-while/brkstat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class brkstat {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i==5){
+                break;
+            }
+            System.out.println(i);
+     
+     }
+       System.out.println("Loop ended");
+    }
+}
---
+++ loop/do-while/mul10.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class mul10
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        do{
+         System.out.print("Enter a number : ");
+         int num=sc.nextInt();
+         if(num % 10 == 0){
+             break;
+         }
+         
+         System.out.println(num);
+    
+     }while(true);
+     System.out.println("Exited the loop.");
+     
+       
+    }
+}
