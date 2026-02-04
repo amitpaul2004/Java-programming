84baed09ce025af9111bd4f12bc7b5ebd0ecdd99
amitpaul2004 <pal900290@gmail.com>
2026-02-04 09:45:14 +0530

    while loop

---
+++ loop/while_loop/prnt1to100.java
@@ -0,0 +1,11 @@
+import  java.util.*;
+public class prnt1to100 {
+    public static void main(String[] args) {
+       int counter = 1; 
+        while (counter <=10) {
+            System.out.println(counter);
+            counter++;
+        }
+        
+    }
+}
